e02b2ad91a261ebffbd3bb0685cb8b079e56b8fb
reathh <Ivelin Kirilov>
2014-10-14 21:46:04 +0300

    Added PHP Exam

---
+++ PHPBasicsExam/03.Text-Gravity.php
@@ -0,0 +1,34 @@
+<?php
+  $text = $_GET['text'];
+$lineLength = $_GET['lineLength'];
+
+$arr = [];
+
+for ($i = 0; $i < strlen($text); $i += $lineLength) {
+    $temp = substr($text, $i, $lineLength);
+    if (strlen($temp) < $lineLength) {
+        $temp .= str_repeat(' ', $lineLength - strlen($temp));
+    }
+    $arr[] = $temp;
+}
+for ($r = 0; $r < $lineLength; $r++) {
+    for ($i = count($arr) - 2; $i >= 0; $i--) {
+        for ($j = 0; $j < strlen($arr[$i]); $j++) {
+            if ($arr[$i + 1][$j] == ' ') {
+                $space = ' ';
+                $arr[$i + 1][$j] = $arr[$i][$j];
+                $arr[$i][$j] = $temp;
+            }
+        }
+    }
+}
+echo("<table>");
+for ($i = 0; $i < count($arr); $i++) {
+    echo("<tr>");
+    for ($j = 0; $j < strlen($arr[$i]); $j++) {
+        echo("<td>" . htmlentities($arr[$i][$j]) . "</td>");
+    }
+    echo("</tr>");
+
+}
+echo("<table>");
